353064abc60ddba7ad55f8f132e7179a55591828
Jose Antonio Riquelme <jariquelme@gmail.com>
2023-03-06 18:13:01 +0100

    Login with google account

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './app.css';
 import { useCallback } from 'react';
 
 import 'survey-core/defaultV2.min.css';
@@ -6,6 +7,10 @@ import { Model } from 'survey-core';
 import { Survey } from 'survey-react-ui';
 import { surveyJson } from "./surveyJson"
 import { GoogleSpreadsheet } from 'google-spreadsheet';
+
+import React, { useState, useEffect } from 'react';
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
 
 const SPREADSHEET_ID = process.env.REACT_APP_PUBLIC_SPREADSHEET_ID;
 const SHEET_ID = process.env.REACT_APP_PUBLIC_SHEET_ID;
@@ -29,6 +34,18 @@ const appendSpreadsheet = async (doc, row) => {
 };
 function App() {
   const survey = new Model(surveyJson);
+  const [ user, setUser ] = useState(null);
+  const [ profile, setProfile ] = useState(null);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log('Login Failed:', error)
+  });
+
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+};
 
   const alertResults = useCallback((sender) => {
     const results = sender.data;
@@ -38,8 +55,42 @@ function App() {
     appendSpreadsheet(doc,results);
   }, []);
 
+  useEffect(
+    () => {
+      console.log("entro", user)
+      if (user) {
+        axios
+          .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: 'application/json'
+            }
+          })
+          .then((res) => {
+              setProfile(res.data);
+          })
+          .catch((err) => console.log(err));
+      }
+    },
+    [ user ]
+);
   survey.onComplete.add(alertResults);
 
+  return (
+    <div>
+        {profile ? (
+            <div>
+              <img src={profile.picture} />
+              <br />
+              <br />
+              <button onClick={logOut}>Log out</button>
+              <Survey model={survey} />
+            </div>
+        ) : (
+            <button className="login-with-google-btn" onClick={() => login()}>Sign in with Google 🚀 </button>
+        )}
+    </div>
+);
   return <Survey model={survey} />;
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const GOOGLE_OAUTH_CLIENT_ID=process.env.REACT_APP_PUBLIC_GOOGLEOAUTH_CLIENT_ID
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId={GOOGLE_OAUTH_CLIENT_ID}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
